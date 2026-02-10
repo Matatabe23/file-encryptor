bc8ec2f8ca52f14fc799b5ef3f193241c6c575b2
QUGOR <93441911+Matatabe23@users.noreply.github.com>
2026-02-10 09:47:11 +0300

    update locale configurations for improved internationalization support

---
+++ app/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+	<v-app>
+		<v-app-bar app>
+			<div class="flex gap-2 ml-auto mr-4 items-center">
+				<LanguageSelect />
+				<ThemeToggle />
+			</div>
+		</v-app-bar>
+
+		<v-main>
+			<slot />
+		</v-main>
+	</v-app>
+</template>
+
+<script setup></script>
