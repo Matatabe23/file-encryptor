861573d29b480bc4db44750b3a9eb6df293a0b0f
QUGOR <93441911+Matatabe23@users.noreply.github.com>
2026-02-14 01:07:14 +0300

    Add Tauri scripts to package.json for enhanced build and development support

---
+++ src-tauri/gen/android/app/src/main/java/ru/qugor/fileencryptor/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.qugor.fileencryptor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
